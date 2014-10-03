e6663f9c73cf5d98cbd1b4259e29ccbf9b915195
Andrejs Stepanovs <a.stepanovs@mysportgroup.de>
2014-10-03 10:19:51 +0200

    reordering container

--- src/Container.php
+++ src/Container.php
@@ -18,107 +18,14 @@ class Container extends Pimple
     {
         $this['config'] = $configData;
 
-        $this['api'] = function ($self) {
-            $googleApi = $self['config']['google-api'];
-            $key = $googleApi['key'];
-            $url = $googleApi['url'];
+        $this->initCalculation();
+        $this->initHelper();
+        $this->initFilterOrder();
+        $this->initApp();
+    }
 
-            return new \App\Google\Elevation\Api($key, $url);
-        };
-
-        $this['helper'] = function ($self) {
-            /** @var \App\Google\Elevation\Api $api */
-            $api = $self['api'];
-
-            $config = $self['config']['google-api'];
-            $locationsInRequest       = $config['locations-in-request'];
-            $allowedRequestsPerSecond = $config['allowed-requests-per-second'];
-
-            return new \App\Helper($api, $locationsInRequest, $allowedRequestsPerSecond);
-        };
-
-        $this['home'] = function ($self) {
-            $config = $self['config']['filter'];
-            $home = empty($config['home']) ? false : $config['home'];
-            if ($home) {
-                return new \App\Location($home['latitude'], $home['longitude'], 0, 'Home');
-            }
-
-            return null;
-        };
-
-        $this['distance'] = function ($self) {
-            $geoLocation = new \AnthonyMartin\GeoLocation\GeoLocation();
-
-            return new \App\Distance($geoLocation);
-        };
-
-        $this['order'] = function ($self) {
-            /** @var \App\Location $home */
-            $home = $self['home'];
-
-            /** @var \App\Distance $url */
-            $distance = $self['distance'];
-
-            return new \App\Order($home, $distance);
-        };
-
-        $this['filter'] = function ($self) {
-            $config      = $self['config']['filter'];
-            $filterCity  = empty($config['city']) ? null : $config['city'];
-            $minDistance = $config['min-distance'];
-            $maxDistance = $config['max-distance'];
-            $maxDistanceFromHome = $config['max-distance-from-home'];
-
-            /** @var \App\Location $home */
-            $home = $self['home'];
-
-            /** @var \App\Distance $url */
-            $distance = $self['distance'];
-
-            return new \App\Filter($distance, $filterCity, $minDistance, $maxDistance, $maxDistanceFromHome, $home);
-        };
-
-        $this['calculation'] = function ($self) {
-
-            /** @var Distance $distance */
-            $distance = $self['distance'];
-            $stops    = $self['config']['filter']['stops'];
-
-            return new \App\Calculation($distance, $stops);
-        };
-
-        $this['stops'] = function ($self) {
-            $config = $self['config']['gtfs'];
-            $file   = $config['path'] . $config['city'] . DIRECTORY_SEPARATOR . 'stops.txt';
-            $stops  = new \Keboola\Csv\CsvFile($file);
-
-            return new \App\Gtfs\Stops($stops);
-        };
-
-        $this['stop-times'] = function ($self) {
-            $config = $self['config']['gtfs'];
-            $file   = $config['path'] . $config['city'] . DIRECTORY_SEPARATOR . 'stop_times.txt';
-            $stopTimes = new \Keboola\Csv\CsvFile($file);
-
-            return new \App\Gtfs\StopTimes($stopTimes);
-        };
-
-        $this['output'] = function ($self) {
-            $url = new \App\Output\Url();
-            $urlHtml = new \App\Output\UrlHtml();
-
-            return new \App\Output\Factory($url, $urlHtml);
-        };
-
-        $this['transfers'] = function ($self) {
-            $config = $self['config']['gtfs'];
-            $file   = $config['path'] . $config['city'] . DIRECTORY_SEPARATOR . 'transfers.txt';
-            $stops  = new \Keboola\Csv\CsvFile($file);
-
-            return new \App\Gtfs\Transfers($stops);
-        };
-
+    private function initApp()
+    {
         $this['app'] = function ($self) {
             /** @var \App\Gtfs\Stops $stops */
             $stops = $self['stops'];
@@ -161,4 +68,113 @@ class Container extends Pimple
         };
     }
 
+    private function initCalculation()
+    {
+        $this['calculation'] = function ($self) {
+
+            /** @var Distance $distance */
+            $distance = $self['distance'];
+            $stops    = $self['config']['filter']['stops'];
+
+            return new \App\Calculation($distance, $stops);
+        };
+
+        $this['stops'] = function ($self) {
+            $config = $self['config']['gtfs'];
+            $file   = $config['path'] . $config['city'] . DIRECTORY_SEPARATOR . 'stops.txt';
+            $stops  = new \Keboola\Csv\CsvFile($file);
+
+            return new \App\Gtfs\Stops($stops);
+        };
+
+        $this['stop-times'] = function ($self) {
+            $config = $self['config']['gtfs'];
+            $file   = $config['path'] . $config['city'] . DIRECTORY_SEPARATOR . 'stop_times.txt';
+            $stopTimes = new \Keboola\Csv\CsvFile($file);
+
+            return new \App\Gtfs\StopTimes($stopTimes);
+        };
+
+        $this['output'] = function ($self) {
+            $url = new \App\Output\Url();
+            $urlHtml = new \App\Output\UrlHtml();
+
+            return new \App\Output\Factory($url, $urlHtml);
+        };
+
+        $this['transfers'] = function ($self) {
+            $config = $self['config']['gtfs'];
+            $file   = $config['path'] . $config['city'] . DIRECTORY_SEPARATOR . 'transfers.txt';
+            $stops  = new \Keboola\Csv\CsvFile($file);
+
+            return new \App\Gtfs\Transfers($stops);
+        };
+    }
+
+    private function initHelper()
+    {
+        $this['api'] = function ($self) {
+            $googleApi = $self['config']['google-api'];
+            $key = $googleApi['key'];
+            $url = $googleApi['url'];
+
+            return new \App\Google\Elevation\Api($key, $url);
+        };
+
+        $this['helper'] = function ($self) {
+            /** @var \App\Google\Elevation\Api $api */
+            $api = $self['api'];
+
+            $config = $self['config']['google-api'];
+            $locationsInRequest       = $config['locations-in-request'];
+            $allowedRequestsPerSecond = $config['allowed-requests-per-second'];
+
+            return new \App\Helper($api, $locationsInRequest, $allowedRequestsPerSecond);
+        };
+
+        $this['distance'] = function ($self) {
+            $geoLocation = new \AnthonyMartin\GeoLocation\GeoLocation();
+
+            return new \App\Distance($geoLocation);
+        };
+    }
+
+    private function initFilterOrder()
+    {
+        $this['home'] = function ($self) {
+            $config = $self['config']['filter'];
+            $home = empty($config['home']) ? false : $config['home'];
+            if ($home) {
+                return new \App\Location($home['latitude'], $home['longitude'], 0, 'Home');
+            }
+
+            return null;
+        };
+
+        $this['order'] = function ($self) {
+            /** @var \App\Location $home */
+            $home = $self['home'];
+
+            /** @var \App\Distance $url */
+            $distance = $self['distance'];
+
+            return new \App\Order($home, $distance);
+        };
+
+        $this['filter'] = function ($self) {
+            $config      = $self['config']['filter'];
+            $filterCity  = empty($config['city']) ? null : $config['city'];
+            $minDistance = $config['min-distance'];
+            $maxDistance = $config['max-distance'];
+            $maxDistanceFromHome = $config['max-distance-from-home'];
+
+            /** @var \App\Location $home */
+            $home = $self['home'];
+
+            /** @var \App\Distance $url */
+            $distance = $self['distance'];
+
+            return new \App\Filter($distance, $filterCity, $minDistance, $maxDistance, $maxDistanceFromHome, $home);
+        };
+    }
 }
